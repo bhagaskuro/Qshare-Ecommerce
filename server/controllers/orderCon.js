d71b1f5ab44f42caaa84317f8cd86484f59e68d3
Bhagaskoro P. Nindyalit <kurohakuro@gmail.com>
2023-09-06 22:28:25 +0700

    feat: createOrder
    adding feature to create new order

--- server/controllers/orderCon.js
+++ server/controllers/orderCon.js
@@ -5,7 +5,7 @@ class Orders {
     const { name, quantity, price } = req.body;
     const id = req.user.id;
     try {
-      const newOrder = {
+      const order = {
         name,
         quantity,
         price,
@@ -13,7 +13,7 @@ class Orders {
       };
 
       //insert into database
-      await Order.create(newOrder);
+      await Order.create(order);
 
       res.status(201).json({ message: `New order has been created` });
     } catch (error) {
